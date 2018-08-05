ab94ec5e8d9ba28cf583723211f455f941727c1e
azrahman <azrahmanZobair99@users.noreply.github.com>
2018-08-06 00:29:57 +0200

    jj
    hhh

--- js/app.js
+++ js/app.js
@@ -1,163 +1,98 @@
-$(document).ready(function () {
-    $('select').formSelect();
-});
-
-
-
-
-
-
-
-
-
-
-
-var myOperation;
-$('#ops').change(function () {
-    switch ($(this).val()) {
-        case 'plus':
-            myOperation = 'plus';
-            $('#num1').attr("placeholder", "First Number");
-            $('#num2').attr("placeholder", "Second Number");
-            $('#num2').prop('disabled', false);
+function zobiCalculate(number1, number2, operation) {
+    switch (operation) {
+        case "plus":
+            return parseFloat(number1) + parseFloat(number2);
             break;
-        case 'minus':
-            myOperation = 'minus';
-            $('#num1').attr("placeholder", "First Number");
-            $('#num2').attr("placeholder", "Second Number");
-            $('#num2').prop('disabled', false);
+        case "minus":
+            return parseFloat(number1) - parseFloat(number2);
             break;
-        case 'multi':
-            myOperation = 'multi';
-            $('#num1').attr("placeholder", "First Number");
-            $('#num2').attr("placeholder", "Second Number");
-            $('#num2').prop('disabled', false);
+        case "multi":
+            return parseFloat(number1) * parseFloat(number2);
             break;
-        case 'devide':
-            myOperation = 'devide';
-            $('#num1').attr("placeholder", "Dividend");
-            $('#num2').attr("placeholder", "Divisor");
-            $('#num2').prop('disabled', false);
+        case "devide":
+            return parseFloat(number1) / parseFloat(number2);
             break;
-        case 'square':
-            myOperation = 'square';
-            $('#num1').attr("placeholder", "Number");
-            $('#num2').attr("placeholder", "");
-            $('#num2').prop('disabled', true);
+        case "square":
+            return Math.sqrt(parseFloat(number1));
             break;
-        case 'power':
-            myOperation = 'power';
-            $('#num1').attr("placeholder", "");
-            $('#num2').attr("placeholder", "Power of n ");
-            $('#num2').prop('disabled', false);
+        case "power":
+            return Math.pow(parseFloat(number1), parseFloat(number2));
             break;
-        case 'percentage':
-            myOperation = 'percentage';
-            $('#num1').attr("placeholder", "Number");
-            $('#num2').attr("placeholder", "Percent %");
-            $('#num2').prop('disabled', false);
+        case "percentage":
+            return (number1 * number2) / 100;
             break;
         default:
             break;
     }
-});
-
+}
 
 
 function executeAction() {
-    switch (myOperation) {
-        case 'plus':
-            $('#zobiResult').val(zobiAdd($('#num1').val(), $('#num2').val()))
+    if (isNaN($('#num1').val()) || isNaN($('#num2').val())) {
+        M.toast({
+            html: 'Please enter a valid number!'
+        });
+    } else {
+        $('#zobiResult').val(zobiCalculate($('#num1').val(), $('#num2').val(), $('#ops').val()));
+
+    }
+}
+
+
+
+
+$(document).ready(function () {
+    $("select").formSelect();
+});
+
+$("#ops").change(function () {
+    switch ($(this).val()) {
+        case "plus":
+            $("#num1").attr("placeholder", "First Number");
+            $("#num2").attr("placeholder", "Second Number");
+            $("#num2").prop("disabled", false);
             break;
-        case 'minus':
-            $('#zobiResult').val(zobiSubstract($('#num1').val(), $('#num2').val()))
+        case "minus":
+            $("#num1").attr("placeholder", "First Number");
+            $("#num2").attr("placeholder", "Second Number");
+            $("#num2").prop("disabled", false);
             break;
-        case 'multi':
-            $('#zobiResult').val(zobiMultiple($('#num1').val(), $('#num2').val()))
+        case "multi":
+            $("#num1").attr("placeholder", "First Number");
+            $("#num2").attr("placeholder", "Second Number");
+            $("#num2").prop("disabled", false);
             break;
-        case 'devide':
-            $('#zobiResult').val(zobiDivide($('#num1').val(), $('#num2').val()))
+        case "devide":
+            $("#num1").attr("placeholder", "Dividend");
+            $("#num2").attr("placeholder", "Divisor");
+            $("#num2").prop("disabled", false);
             break;
-        case 'square':
-            $('#zobiResult').val(zobiSquares($('#num1').val()));
+        case "square":
+            $("#num1").attr("placeholder", "Number");
+            $("#num2").attr("placeholder", "");
+            $("#num2").prop("disabled", true);
             break;
-        case 'power':
-            $('#zobiResult').val(zobiPowers($('#num1').val(), $('#num2').val()))
+        case "power":
+            $("#num1").attr("placeholder", "");
+            $("#num2").attr("placeholder", "Power of n ");
+            $("#num2").prop("disabled", false);
             break;
-        case 'percentage':
-            $('#zobiResult').val(zobiPercentage($('#num1').val(), $('#num2').val()))
+        case "percentage":
+            $("#num1").attr("placeholder", "Number");
+            $("#num2").attr("placeholder", "Percent %");
+            $("#num2").prop("disabled", false);
             break;
         default:
             break;
     }
-}
+});
 
 
 
 
-function zobiPercentage(number1, percent) {
-    if (isNaN(number1) || isNaN(percent)) {
-        alert("Please enter a valid decimal number");
-    } else {
-        var result;
-        result = (number1 * percent) / 100;
-        return result;
-    }
-}
 
-
-
-function zobiAdd(number1, number2) {
-    if (isNaN(number1) || isNaN(number2)) {
-        alert("Please enter a valid decimal number");
-    } else {
-        return parseFloat(number1) + parseFloat(number2);
-    }
-}
-
-function zobiSubstract(number1, number2) {
-    if (isNaN(number1) || isNaN(number2)) {
-        alert("please enter a valid decimal number");
-    } else {
-        return parseFloat(number1) - parseFloat(number2);
-    }
-}
-
-
-function zobiMultiple(number1, number2) {
-    if (isNaN(number1) || isNaN(number2)) {
-        alert("please enter a valid decimal number");
-    } else {
-        return parseFloat(number1) * parseFloat(number2);
-    }
-}
-
-function zobiDivide(number1, number2) {
-    if (isNaN(number1) || isNaN(number2)) {
-        alert("please enter a valid decimal number");
-    } else {
-        return parseFloat(number1) / parseFloat(number2)
-    }
-}
-
-function zobiSquares(number1) {
-    if (isNaN(number1)) {
-        alert("enter a valid number");
-    } else {
-        return Math.sqrt(parseFloat(number1));
-    }
-}
-
-function zobiPowers(number1, numtoPower) {
-    if (isNaN(number1) || isNaN(numtoPower)) {
-        alert("enter a valid number");
-    } else {
-        return Math.pow(parseFloat(number1), parseFloat(numtoPower));
-    }
-}
-
-$('#btnClear').click(function () {
-    $('#zobiResult').val('');
-    $('#num1').val('');
-    $('#num2').val('');
+$("#btnClear").click(function () {
+    $("#zobiResult").val("");
+    $("#num1").val("");
+    $("#num2").val("");
 });
